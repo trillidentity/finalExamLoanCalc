b8357959e476437ed9d818bd2b3f13df81acfe81
oluwaseyiosinubi <oluwaseyiosinubi@wifi-roaming-128-4-74-6.host.udel.edu>
2019-12-13 00:18:52 -0500

    updated some stuffs, still working on tests

--- StudentLoanCalc/src/main/java/app/controller/LoanCalcViewController.java
+++ StudentLoanCalc/src/main/java/app/controller/LoanCalcViewController.java
@@ -1,13 +1,13 @@
 package app.controller;
 
 import app.StudentCalc;
-import app.LoanCalcHelper;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import pkgLogic.Loan;
 import pkgLogic.Payment;
 
 import java.net.URL;
@@ -23,8 +23,6 @@ public class LoanCalcViewController implements Initializable   {
 
 	private StudentCalc SC = null;
 	
-	private LoanCalcHelper loanCalcHelper = null;
-	
 	@FXML
 	private TextField LoanAmount;
 	
@@ -38,13 +36,14 @@ public class LoanCalcViewController implements Initializable   {
 	private DatePicker PaymentStartDate;
 	
 	@FXML
-	private Label lblTotalPayments;
-
-	@FXML
 	private TextField AdditionalPayment;
-
+	
+	@FXML
+	private Label lblTotalPayments;
+	
 	@FXML
 	private Label lblTotalInterest;
+
 	
 	@FXML
 	private TableView<Payment> tvResults;
@@ -53,34 +52,42 @@ public class LoanCalcViewController implements Initializable   {
 	private TableColumn<Payment, Integer> colPaymentNumber;
 	
 	@FXML
-	private Label ratePerMonth;
-
+	private TableColumn<Payment, LocalDate> colDueDate;
+	
+	@FXML
+	private TableColumn<Payment, Double> colPayment;
+	
+	@FXML 
+	private TableColumn<Payment, Double> colAdditionalPayment;
+	
+	@FXML
+	private TableColumn<Payment, Double> colInterest;
+	
+	@FXML
+	private TableColumn<Payment, Double> colPrinciple;
+	
+	@FXML
+	private TableColumn<Payment, Double> colBalance;
+	
 	private ObservableList<Payment> paymentList = FXCollections.observableArrayList();
 	
-	//TODO: Account for all the other columns	
-	@FXML
-	public TableColumn<Payment, String> paymentNbr, dueDate, payment, additionalPayment, interest, principle, balance;
-	
+	//TODO: Account for all the other columns		
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		colPaymentNumber.setCellValueFactory(new PropertyValueFactory<>("paymentNbr"));
-		//TODO: Add a 'setCellValueFactor' entry for each column, mapping to each attribute in Payment
+		colDueDate.setCellValueFactory(new PropertyValueFactory<>("dueDate"));
+		colPayment.setCellValueFactory(new PropertyValueFactory<>("payment"));
+		colAdditionalPayment.setCellValueFactory(new PropertyValueFactory<>("additionalPayment"));
+		colInterest.setCellValueFactory(new PropertyValueFactory<>("interest"));
+		colPrinciple.setCellValueFactory(new PropertyValueFactory<>("principle"));
+		colBalance.setCellValueFactory(new PropertyValueFactory<>("balance"));
+		//TODO: Add a 'setCellValueFactory' entry for each column, mapping to each attribute in Payment
 		
-		dueDate.setCellValueFactory(new PropertyValueFactory<>("DueDate"));
-		payment.setCellValueFactory(new PropertyValueFactory<>("Payment"));
-		additionalPayment.setCellValueFactory(new PropertyValueFactory<>("AdditionalPayment"));
-		interest.setCellValueFactory(new PropertyValueFactory<>("Interest"));
-		principle.setCellValueFactory(new PropertyValueFactory<>("Principle"));
-		balance.setCellValueFactory(new PropertyValueFactory<>("Balance"));
 		tvResults.setItems(paymentList);
 	}
 
 	public void setMainApp(StudentCalc sc) {
 		this.SC = sc;
-	}
-	
-	public void setCalcHelper(LoanCalcHelper lc) {
-		this.loanCalcHelper = lc;
 	}
 	
 	/**
@@ -89,34 +96,36 @@ public class LoanCalcViewController implements Initializable   {
 	 * @version 1.0
 	 * @param event
 	 */
+	
+	
+	
 	@FXML
 	private void btnCalcLoan(ActionEvent event) {
-
-		//	Examples- how to read data from the form
-		double dLoanAmount = Double.parseDouble(LoanAmount.getText());		
-		lblTotalPayments.setText("123");		
-		LocalDate localDate = PaymentStartDate.getValue();
 		
-		loanCalcHelper.calculateLoan(dLoanAmount, numberOfYears, annualInterestRate, startDate, additionalPayment);
+		tvResults.getItems().clear();
+		lblTotalPayments.setText("");
+		lblTotalInterest.setText("");
+		
+		Loan loan = new Loan(PaymentStartDate.getValue(), Double.parseDouble(LoanAmount.getText()),
+				Double.parseDouble(InterestRate.getText()), Double.parseDouble(AdditionalPayment.getText()),
+				Integer.parseInt(NbrOfYears.getText())*12);
+		
+		paymentList.addAll(loan.getLoanPayments());
+		tvResults.setItems(paymentList);
+		
+		lblTotalPayments.setText(Double.toString(loan.getTotalPayments()));
+		lblTotalInterest.setText(Double.toString(loan.getTotalInterest()));
+			 
 
-		Payment[] paymentDetails = loanCalcHelper.getMonthlyPaymentDetails();
-
-		double totalPayments = loanCalcHelper.getTotalPayment();
-		lblTotalPayments.setText("" + totalPayments);
-
-		double totalInterest = loanCalcHelper.getTotalInterest();
-		lblTotalInterest.setText("" + totalInterest);
-
-		double monthlyRate = loanCalcHelper.getMonthlyRate();
-		ratePerMonth.setText(monthlyRate + "%");
-
-		ObservableList<Payment> paymentDetailsList = FXCollections.observableArrayList();
-		 for (Payment pd: paymentDetails) {
-		 	paymentDetailsList.addAll(pd);
-		 }
-		 tvResults.setItems(paymentDetailsList);
-
-		loanCalcHelper.reset();
+		
+		//	Examples- how to read data from the form
+		/*double dLoanAmount = Double.parseDouble(LoanAmount.getText());		
+		lblTotalPayments.setText("240");
+		lblTotalInterest.setText("29,000");
+		LocalDate localDate = PaymentStartDate.getValue();
+		*/
+		
+		
 		/*
 		 * When this button is clicked, you need to do the following:
 		 * 
@@ -131,17 +140,7 @@ public class LoanCalcViewController implements Initializable   {
 		 * Additional Payment - based on Additional Payment given by user
 		 * Interest - Calculate based on 
 		 */
+		
 	}
-	@FXML
-	private void btnReset(ActionEvent event) {
-		LoanAmount.clear();
-		InterestRate.clear();
-		NbrOfYears.clear();
-		PaymentStartDate.setValue(null);
-		tvResults.getItems().clear();
-		loanCalcHelper.reset();
-		lblTotalPayments.setText("");
-		lblTotalInterest.setText("");
-		ratePerMonth.setText("");
-	}
+	
 }
